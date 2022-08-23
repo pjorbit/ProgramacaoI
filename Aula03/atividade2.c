e5945afccca31f2324da1737f5dcbd4cf568aa07
Rafael Soares <rafael-1440973@educar.rs.gov.br>
2022-08-23 20:35:14 -0300

    Aula 03 concluida

--- Aula03/atividade2.c
+++ Aula03/atividade2.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <locale.h>
-#include <math.h>
 
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 
-	
+	char nome[30], vogal[5], consoante[5];
+
+	printf("Por favor, entre com seu nome: \n");
+	scanf("%s", nome);
+	printf("Por favor, entre com uma VOGAL: \n");
+	scanf("%s", vogal);
+	scanf("%s", consoante);
+	printf("%s \n", nome);
+	printf("Sua vogal é: %s \n", vogal);
+	printf("Sua consoante é: %s", consoante);
+
+	return 0;
 }
